71be61b9bcfcc56549fbc8bc79d8685e9c082d64
Jean <jeansueiro60@gmail.com>
2024-11-22 00:45:59 -0300

    update package

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const routes = require("./routes");
-
+const PORT = process.env.APP_PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(process.env.APP_PORT, () =>
-  console.log(
-    `Servidor corriendo en http://localhost:${process.env.APP_PORT}.\n`
-  )
+app.listen(PORT, () =>
+  console.log(`Servidor corriendo en http://localhost:${PORT}.\n`)
 );
 
 module.exports = app;
