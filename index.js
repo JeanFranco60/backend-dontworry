6126dc525d3b222eef53b20cf69f39b3b034f78f
Jean <jeansueiro60@gmail.com>
2024-11-22 03:52:14 -0300

    cors changed

--- index.js
+++ index.js
@@ -4,19 +4,11 @@ const cors = require("cors");
 const app = express();
 const routes = require("./routes");
 
-// Configuración de CORS
-const corsOptions = {
-  origin: "https://dontuworry.netlify.app/", // Cambia esta URL por la de tu frontend si está en otro dominio
-  methods: "GET,POST,PUT,DELETE", // Métodos permitidos
-  allowedHeaders: "Content-Type,Authorization", // Encabezados permitidos
-};
-
-// Usar CORS con las opciones configuradas
-app.use(cors(corsOptions));
-
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(PORT, () => {
