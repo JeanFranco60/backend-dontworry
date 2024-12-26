35846c93a30b188733751c10d7f665a932e73b1c
Jean <jeansueiro60@gmail.com>
2024-12-26 01:13:06 -0300

    refactor seeders to run in parallel and improve error handling

--- seeders/index.js
+++ seeders/index.js
@@ -4,9 +4,14 @@ const categorySeeder = require("./categorySeeders");
 const userSeeder = require("./userSeeders");
 
 async function run() {
-  await categorySeeder();
-  await productSeeder();
-  await userSeeder;
-  console.log("Seeders has been ran");
+  try {
+    // Ejecutar los seeders en paralelo si no importa el orden
+    await Promise.all([userSeeder(), categorySeeder(), productSeeder()]);
+
+    console.log("Seeders have been run successfully.");
+  } catch (error) {
+    console.error("Error running seeders:", error);
+  }
 }
+
 run();
--- seeders/userSeeders.js
+++ seeders/userSeeders.js
@@ -9,7 +9,7 @@ async function userSeeder() {
     email: "user@project.com",
     address: "michilandia",
     phone: 123456789,
-    password: await bcrypt.hash("1234", 10),
+    password: 1234,
   };
   const users = [];
   users.push(firstUser);
