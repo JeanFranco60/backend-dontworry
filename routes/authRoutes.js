12adb112f5bc7a957524724d61b8202e8a608aee
Jean <jeansueiro60@gmail.com>
2025-01-03 23:02:32 -0300

    refactor authController and authRoutes to streamline token generation and improve code clarity

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,40 +1,7 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const { User } = require("../models");
 const router = express.Router();
+const authController = require("../controllers/authController");
 
-// Configuración de clave secreta
-const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret_key";
-
-// Endpoint de login
-router.post("/users/validate", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    // Verificar si el usuario existe
-    const user = await User.findOne({ where: { email } });
-
-    if (!user) {
-      return res.status(404).json({ error: "Usuario no encontrado" });
-    }
-
-    // Verificar la contraseña
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      return res.status(401).json({ error: "Contraseña incorrecta" });
-    }
-
-    // Generar un token JWT
-    const token = jwt.sign({ id: user.id, role: user.role }, TOKEN_SECRET, {
-      expiresIn: "1h", // Token válido por 1 hora
-    });
-
-    return res.json({ token, role: user.role });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Error del servidor" });
-  }
-});
+router.post("/user/validate", authController.getToken);
 
 module.exports = router;
