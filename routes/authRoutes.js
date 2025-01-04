66be8bad236ed2f048e49f93c39947a2d8e68c24
Jean <jeansueiro60@gmail.com>
2025-01-03 23:14:32 -0300

    rename user validation route to /tokens for clarity and consistency

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-router.post("/user/validate", authController.getToken);
+router.post("/tokens", authController.getToken);
 
 module.exports = router;
