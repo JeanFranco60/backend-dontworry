7eb79c57efc03f4b1b602726078b52a9fd3a448e
Jean <jeansueiro60@gmail.com>
2025-01-20 23:07:14 -0300

    update Stripe package to version 17.5.0 and add new API resources

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,15 @@ const userRoutes = require("./userRoutes");
 const authRoutes = require("./authRoutes");
 const adminRoutes = require("./adminRoutes");
 
+const stripeRoutes = require("./stripeRoutes");
+
 router.use("/admins", adminRoutes);
 router.use("/products", productRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
+router.use("/stripe", stripeRoutes);
+
 router.use(function (req, res) {
   res.status(404).json({ errors: ["Endpoint not found"] });
 });
